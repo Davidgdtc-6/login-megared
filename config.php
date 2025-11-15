716078e5e844b7c3388d1d860372968fb56bb4f0
David Torres <gustavotorresc2001@gmail.com>
2025-11-14 21:08:21 -0500

    Añadir config.php para entrega final

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php
+
+// Constantes de la Base de Datos
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'db-megared');
+
+?>
